240d195467b2241346000c8cc2597469d8bac16e
xoxoisme <kxoxo3333@gmail.com>
2025-11-13 22:58:41 +0900

    feat: PointControllerTest 내 포인트 충전 테스트 기능 추가

--- backend/src/test/java/org/example/backend/domain/point/controller/PointControllerTest.java
+++ backend/src/test/java/org/example/backend/domain/point/controller/PointControllerTest.java
@@ -7,6 +7,8 @@ import org.example.backend.domain.point.repository.PointRepository;
 import org.example.backend.domain.trade.repository.TradeRepository;
 import org.example.backend.global.LoginUtil;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +17,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -50,6 +56,16 @@ public class PointControllerTest {
         );
         testMember = loginUtil.getMemberByEmail("point@test.com");
     }
+
+    @Test
+    @DisplayName("t1: 포인트 충전")
+    void chargePoint() throws Exception {
+        mvc.perform(post("/api/points/members/charge/{amount}", 5000)
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value("포인트 충전 완료"));
+    }
+}
 
 
 
