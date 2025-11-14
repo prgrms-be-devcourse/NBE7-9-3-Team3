93ad0a463cebb8099e3e014462ce441b3bbac966
qivvoon <gangjiwon70@gmail.com>
2025-11-14 14:39:50 +0900

    refactor: 어항 도메인 필요없는 롬복 제거

--- backend/src/main/java/org/example/backend/domain/aquarium/dto/AquariumRequestDto.kt
+++ backend/src/main/java/org/example/backend/domain/aquarium/dto/AquariumRequestDto.kt
@@ -3,7 +3,6 @@ package org.example.backend.domain.aquarium.dto
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
-@JvmRecord
 data class AquariumRequestDto(
     @JvmField val aquariumName: @NotNull @Size(min = 1, max = 50) String
 )
