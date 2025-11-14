39217b081166862cc31859a58b72c8c8a4024d7e
qivvoon <gangjiwon70@gmail.com>
2025-11-14 15:46:28 +0900

    refactor: 물고기 도메인 코틀린 마이그레이션

---
+++ backend/src/main/java/org/example/backend/domain/fish/entity/Fish.kt
@@ -0,0 +1,32 @@
+package org.example.backend.domain.fish.entity
+
+import jakarta.persistence.*
+import lombok.AccessLevel
+import lombok.Getter
+import lombok.NoArgsConstructor
+import org.example.backend.domain.aquarium.entity.Aquarium
+import org.example.backend.global.jpa.entity.BaseEntity
+
+@Entity
+class Fish(
+    @field:JoinColumn(name = "aquarium_id")
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    var aquarium: Aquarium,
+
+    @field:Column(length = 50)
+    var species: String,
+
+    @field:Column(length = 50)
+    var name: String
+
+) : BaseEntity() {
+
+    fun changeAquarium(myOwnedAquarium: Aquarium) {
+        this.aquarium = myOwnedAquarium
+    }
+
+    fun changeDetails(species: String, name: String) {
+        this.species = species
+        this.name = name
+    }
+}
