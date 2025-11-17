9462c9f5c3bb30dc4844440833c4877cb10d23ce
do04080 <116405072+do04080@users.noreply.github.com>
2025-11-17 11:18:26 +0900

    Restore: 복구

--- backend/src/test/java/org/example/backend/global/PointUtil.java
+++ backend/src/test/java/org/example/backend/global/PointUtil.java
@@ -26,12 +26,7 @@ public class PointUtil {
 
     // 판매자 생성
     public Member createSeller() {
-        Member seller = Member.builder()
-                .email("seller@test.com")
-                .password(passwordEncoder.encode("seller1234"))
-                .nickname("seller")
-                .profileImage("")
-                .build();
+        Member seller = new Member("seller@test.com", passwordEncoder.encode("seller1234"),"seller", "");
 
         return memberRepository.save(seller);
     }
