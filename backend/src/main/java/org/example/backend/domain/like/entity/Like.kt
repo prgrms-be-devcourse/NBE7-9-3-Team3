039c6114f7cec585286d48b8976bee5d6e6c8ee3
bonangk <rnqhsghkdsla@gmail.com>
2025-11-17 15:49:18 +0900

    feat: Like 마이그레이션

--- backend/src/main/java/org/example/backend/domain/like/entity/Like.kt
+++ backend/src/main/java/org/example/backend/domain/like/entity/Like.kt
@@ -1,40 +1,24 @@
-package org.example.backend.domain.like.entity;
+package org.example.backend.domain.like.entity
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.example.backend.domain.member.entity.Member;
-import org.example.backend.domain.post.entity.Post;
-import org.example.backend.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*
+import lombok.Getter
+import lombok.NoArgsConstructor
+import org.example.backend.domain.member.entity.Member
+import org.example.backend.domain.post.entity.Post
+import org.example.backend.global.jpa.entity.BaseEntity
 
 @Entity
 @NoArgsConstructor
 @Getter
-@Table(
-    name = "likes",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"post_id", "user_id"})
-    }
-)
-public class Like extends BaseEntity {
+@Table(name = "likes",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["post_id", "user_id"])])
+class Like(
 
+    @field:JoinColumn(name = "user_id", nullable = false)
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    var member: Member,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private Member member;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
-
-    public Like(Member member, Post post) {
-        this.member = member;
-        this.post = post;
-    }
-
-}
+    @field:JoinColumn(name = "post_id", nullable = false)
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    var post: Post
+) : BaseEntity()
