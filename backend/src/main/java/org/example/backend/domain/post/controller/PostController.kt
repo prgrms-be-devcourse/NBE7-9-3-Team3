df2643439132420f4141730cee856f89836cd207
bonangk <rnqhsghkdsla@gmail.com>
2025-11-17 14:43:30 +0900

    feat: PostController 까지 마이그레이션

--- backend/src/main/java/org/example/backend/domain/post/controller/PostController.kt
+++ backend/src/main/java/org/example/backend/domain/post/controller/PostController.kt
@@ -1,121 +1,96 @@
-package org.example.backend.domain.post.controller;
+package org.example.backend.domain.post.controller
 
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.example.backend.domain.post.dto.FilterType;
-import org.example.backend.domain.post.dto.MyPostReadResponseDto;
-import org.example.backend.domain.post.dto.PostListResponseDto;
-import org.example.backend.domain.post.dto.PostModifyRequestDto;
-import org.example.backend.domain.post.dto.PostReadResponseDto;
-import org.example.backend.domain.post.dto.PostResponseDto;
-import org.example.backend.domain.post.dto.PostWriteRequestDto;
-import org.example.backend.domain.post.entity.Post.BoardType;
-import org.example.backend.domain.post.entity.Post.Category;
-import org.example.backend.domain.post.service.PostService;
-import org.example.backend.global.response.ApiResponse;
-import org.example.backend.global.security.CustomUserDetails;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor
+import org.example.backend.domain.post.dto.*
+import org.example.backend.domain.post.entity.Post
+import org.example.backend.domain.post.service.PostService
+import org.example.backend.global.response.ApiResponse
+import org.example.backend.global.security.CustomUserDetails
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
-public class PostController implements PostControllerSpec {
+class PostController (
+    private val postService: PostService
+) : PostControllerSpec{
 
-    private final PostService postService;
 
-    @Override
     @GetMapping("/my")
-    public ApiResponse<List<MyPostReadResponseDto>> getMyPosts(
-        @RequestParam BoardType boardType,
-        @AuthenticationPrincipal CustomUserDetails userDetails
-    ) {
+    override fun getMyPosts(
+        @RequestParam boardType: Post.BoardType,
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ApiResponse<List<MyPostReadResponseDto>> {
 
-        List<MyPostReadResponseDto> response = postService.getMyPosts(boardType, userDetails.getId());
+        val response = postService.getMyPosts(boardType, userDetails.id!!)
 
-        return ApiResponse.ok("내가 쓴 게시글 다건 조회", response);
+        return ApiResponse.ok("내가 쓴 게시글 다건 조회", response)
     }
 
-    @Override
     @GetMapping
-    public ApiResponse<PostListResponseDto> getPosts(
-        @RequestParam BoardType boardType,
-        @RequestParam(defaultValue = "ALL") FilterType filterType, // "ALL" or "FOLLOWING"
-        @AuthenticationPrincipal CustomUserDetails userDetails,
-        @RequestParam(required = false) String keyword,
-        @RequestParam(defaultValue = "ALL") Category category,
-        @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
-    ) {
+    override fun getPosts(
+        @RequestParam boardType: Post.BoardType,
+        @RequestParam(defaultValue = "ALL") filterType: FilterType,  // "ALL" or "FOLLOWING"
+        @AuthenticationPrincipal userDetails: CustomUserDetails,
+        @RequestParam(required = false) keyword: String?,
+        @RequestParam(defaultValue = "ALL") category: Post.Category,
+        @PageableDefault(
+            size = 10,
+            sort = ["id"],
+            direction = Sort.Direction.DESC
+        ) pageable: Pageable
+    ): ApiResponse<PostListResponseDto> {
 
-        PostListResponseDto response = postService.getPosts(
-            boardType, filterType, userDetails.getMember(), keyword, category, pageable
-        );
+        val response = postService.getPosts(
+            boardType, filterType, userDetails.member, keyword, category, pageable
+        )
 
-        return ApiResponse.ok("게시글 다건 조회", response);
+        return ApiResponse.ok("게시글 다건 조회", response)
     }
 
-    @Override
     @GetMapping("/{id}")
-    public ApiResponse<PostReadResponseDto> getPost(
-        @PathVariable Long id,
-        @AuthenticationPrincipal CustomUserDetails userDetails
-    ) {
+    override fun getPost(
+        @PathVariable id: Long,
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ApiResponse<PostReadResponseDto> {
+        val response = postService.getPostById(id, userDetails.member)
 
-        PostReadResponseDto response = postService.getPostById(id, userDetails.getMember());
 
-
-        return ApiResponse.ok("%d번 게시글 단건 조회".formatted(id), response);
-
+        return ApiResponse.ok("${id}번 게시글 단건 조회", response)
     }
 
-    @Override
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> deletePost(
-        @PathVariable Long id,
-        @AuthenticationPrincipal CustomUserDetails userDetails
-    ) {
+    override fun deletePost(
+        @PathVariable id: Long,
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ApiResponse<Void> {
+        postService.delete(id, userDetails.member)
 
-        postService.delete(id, userDetails.getMember());
-
-        return ApiResponse.ok("%d번 게시글 삭제".formatted(id));
-
+        return ApiResponse.ok("${id}번 게시글 삭제")
     }
 
-    @Override
     @PostMapping
-    public ApiResponse<PostResponseDto> createPost(
-        @RequestBody PostWriteRequestDto reqBody,
-        @AuthenticationPrincipal CustomUserDetails userDetails
-    ) {
+    override fun createPost(
+        @RequestBody reqBody: PostWriteRequestDto,
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ApiResponse<PostResponseDto> {
+        val response = postService.write(reqBody, userDetails.member)
 
-        PostResponseDto response = postService.write(reqBody, userDetails.getMember());
-
-        return ApiResponse.ok("게시글 생성",response);
-
+        return ApiResponse.ok("게시글 생성", response)
     }
 
-    @Override
     @PatchMapping("/{id}")
-    public ApiResponse<PostResponseDto> modifyPost(
-        @PathVariable Long id,
-        @RequestBody PostModifyRequestDto reqBody,
-        @AuthenticationPrincipal CustomUserDetails userDetails
-    ) {
+    override fun modifyPost(
+        @PathVariable id: Long,
+        @RequestBody reqBody: PostModifyRequestDto,
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ApiResponse<PostResponseDto> {
+        val response = postService.modify(id, reqBody, userDetails.member)
 
-        PostResponseDto response = postService.modify(id, reqBody, userDetails.getMember());
-
-        return ApiResponse.ok("%d번 게시글 수정".formatted(id),response);
+        return ApiResponse.ok("${id}번 게시글 수정", response)
     }
-
 }
