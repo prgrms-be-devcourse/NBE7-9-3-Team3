1bc4e03adda8604e38239ba629100973448a0400
[BE9]도석환 <do04080@gmail.com>
2025-11-17 17:36:07 +0900

    refactor: follow Kotlin전환

--- backend/src/main/java/org/example/backend/domain/follow/dto/FollowResponseDto.kt
+++ backend/src/main/java/org/example/backend/domain/follow/dto/FollowResponseDto.kt
@@ -2,6 +2,6 @@ package org.example.backend.domain.follow.dto
 
 data class FollowResponseDto(
     val memberId: Long?,
-    val nickname: String?,
+    val nickname: String,
     val profileImage: String?
 )
